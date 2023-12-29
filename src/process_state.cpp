60da372836fa239f6d22a0933a8473d0ac9a48e1
keshprad <32313895+keshprad@users.noreply.github.com>
2023-12-28 16:46:52 -0800

    print process state

--- src/process_state.cpp
+++ src/process_state.cpp
@@ -1,4 +1,19 @@
 #include "process_state.h"
+#include <ostream>
 
 ProcessState::ProcessState() {}
-ProcessState::ProcessState(int pid) : m_Pid(pid) {}
+ProcessState::ProcessState(int pid, int arrival_time)
+    : m_Pid(pid),
+      // initialize last timestamp executed on CPU to the arrival time
+      m_LastCpuTime(arrival_time) {}
+
+std::ostream &operator<<(std::ostream &os, const ProcessState &obj) {
+  os << "ProcessState: {\n"
+     << "\tpid: " << obj.m_Pid << ",\n"
+     << "\tlast_cpu_time: " << obj.m_LastCpuTime << ",\n"
+     << "\ttotal_cpu_time: " << obj.m_TotalCpuTime << ",\n"
+     << "\tturnaround_time: " << obj.m_TurnaroundTime << ",\n"
+     << "\twaiting_time: " << obj.m_WaitingTime << ",\n"
+     << '}';
+  return os;
+}
--- src/process_state.h
+++ src/process_state.h
@@ -1,19 +1,29 @@
 #ifndef PROCESS_STATE_H
 #define PROCESS_STATE_H
 
+#include <ostream>
+
 // Provides info about state of a process added to a CPU scheduling algorithm
 class ProcessState {
 private:
   int m_Pid;
 
 public:
-  int m_WaitingTime = 0;
+  // last timestamp process was executed on cpu.
+  // initialized to process arrival time
+  int m_LastCpuTime;
+  // total time process has spent executing
+  int m_TotalCpuTime = 0;
+  // time between process arrival and completion
   int m_TurnaroundTime = 0;
-  int m_CPUTime = 0;
+  // total time process has spent waiting
+  int m_WaitingTime = 0;
 
   ProcessState();
-  ProcessState(int pid);
+  ProcessState(int pid, int arrival_time);
   int getPid() const { return m_Pid; }
+
+  friend std::ostream &operator<<(std::ostream &os, const ProcessState &obj);
 };
 
 #endif
