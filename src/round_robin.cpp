dce9a897a8caaf3b417a780cc78312de190e984f
keshprad <32313895+keshprad@users.noreply.github.com>
2023-12-29 17:28:53 -0800

    round robin implementation

---
+++ src/round_robin.cpp
@@ -0,0 +1,53 @@
+#include "round_robin.h"
+#include <algorithm>
+
+const std::string RoundRobin::m_Name = "RoundRobin";
+
+RoundRobin::RoundRobin() : m_Quantum(1) {}
+RoundRobin::RoundRobin(double quantum) : m_Quantum(quantum) {}
+
+// adds process to necessary datastructures to schedule process
+void RoundRobin::scheduleProcess(Process proc) {
+  // add to ready priority queue
+  m_ReadyMinHeap.push(proc);
+}
+
+void RoundRobin::exec() {
+  while (!m_ReadyMinHeap.empty()) {
+    // get top element
+    auto proc = m_ReadyMinHeap.top();
+    // remove top element
+    m_ReadyMinHeap.pop();
+
+    // calculate start time for current proc: either the prev event end time or
+    // curr proc arrival time
+    double start_time = std::max(
+        m_Events.empty() ? 0 : m_Events[m_Events.size() - 1].getEndTime(),
+        proc.getArrivalTime());
+    // find time executed thus far
+    double cpuTime = m_ProcessStates[proc.getPid()].m_TotalCpuTime;
+    // calculate end time
+    // exec time is either quantum, or rest of time remaining to complete
+    // process
+    double end_time =
+        start_time + std::min(m_Quantum, proc.getBurstTime() - cpuTime);
+
+    // create and push event
+    Event e(proc.getPid(), start_time, end_time);
+    m_Events.push_back(e);
+
+    // update process state
+    ProcessState &ps = m_ProcessStates[proc.getPid()];
+    ps.m_TotalCpuTime += end_time - start_time;
+    ps.m_WaitingTime += start_time - ps.m_LastCpuTime;
+    ps.m_TurnaroundTime = end_time - proc.getArrivalTime();
+    ps.m_LastCpuTime = end_time;
+
+    // check if proc needs to go back into heap
+    // Define a small epsilon value for floating point comparison
+    double epsilon = 1e-10;
+    if (ps.m_TotalCpuTime < proc.getBurstTime() - epsilon) {
+      scheduleProcess(proc);
+    }
+  }
+}
---
+++ src/round_robin.h
@@ -0,0 +1,33 @@
+#ifndef ROUND_ROBIN_H
+#define ROUND_ROBIN_H
+
+#include "cpu_scheduler.h"
+#include "process.h"
+#include <functional>
+#include <queue>
+#include <vector>
+
+class MinHeapProcessComparator {
+public:
+  bool operator()(Process &a, Process &b) {
+    return !Process::priorityComparator(a, b);
+  }
+};
+
+class RoundRobin : public CPUScheduler {
+private:
+  static const std::string m_Name;
+  const double m_Quantum;
+
+  std::priority_queue<Process, std::vector<Process>, MinHeapProcessComparator>
+      m_ReadyMinHeap;
+
+public:
+  RoundRobin();
+  RoundRobin(double quantum);
+
+  const std::string &getName() const override { return m_Name; }
+  void scheduleProcess(Process proc) override;
+  void exec() override;
+};
+#endif
